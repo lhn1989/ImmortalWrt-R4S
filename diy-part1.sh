aa7cc4bb5cf19fe0d71ec1bd8323e7408e963c68
lhn1989 <lhn1989@users.noreply.github.com>
2022-06-01 18:37:02 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,12 +10,6 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# openclash var
-OPENCLASH_DIR="openclash"
-OPENCLASH_GIT="https://github.com/vernesong/OpenClash.git"
-OPENCLASH_SRC="luci-app-openclash"
-OPENCLASH_BRANCH="dev"
-
 # aliyundrive var
 ALIYUNDRIVE_DIR="aliyundrive"
 ALIYUNDRIVE_GIT="https://github.com/messense/aliyundrive-fuse.git"
@@ -36,14 +30,6 @@ shallowClone(){
 # Print Commands
 set -x
 
-# Add SSR
-echo "src-git helloworld https://github.com/fw876/helloworld" >>feeds.conf.default
-
 # Add aliyundrive
 shallowClone $ALIYUNDRIVE_DIR $ALIYUNDRIVE_GIT $ALIYUNDRIVE_SRC $ALIYUNDRIVE_BRANCH
 echo "src-link aliyundrive /workdir/$ALIYUNDRIVE_DIR/$ALIYUNDRIVE_SRC" >> /workdir/openwrt/feeds.conf.default
-
-# Add openclash
-shallowClone $OPENCLASH_DIR $OPENCLASH_GIT $OPENCLASH_SRC $OPENCLASH_BRANCH
-mv /workdir/$OPENCLASH_DIR/$OPENCLASH_SRC /workdir/openwrt/package/
-rm -rf /workdir/$OPENCLASH_DIR
